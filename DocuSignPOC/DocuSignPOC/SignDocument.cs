137e76cbe687facbc6f93b0bd89d16cb6984f550
anuragkalra86 <anurag_mait2004@yahoo.co.in>
2015-08-03 14:37:31 -0400

    created empty project

---
+++ DocuSignPOC/DocuSignPOC/SignDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocuSignPOC
+{
+    public class SignDocument
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DocuSignPOC/HardCodedDocuSignAdapter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DocuSignPOC;
+
+namespace HardCodedDocuSignAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SignDocument.Test();
+            Console.ReadKey();
+        }
+    }
+}
