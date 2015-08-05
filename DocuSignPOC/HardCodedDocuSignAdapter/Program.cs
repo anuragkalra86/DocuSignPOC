67b783bb7482b921065d308d7fb59a17d7b7dfbf
Taigir <ronnie.davies@safeauto.com>
2015-08-05 11:46:34 -0400

    no change

--- DocuSignPOC/HardCodedDocuSignAdapter/Program.cs
+++ DocuSignPOC/HardCodedDocuSignAdapter/Program.cs
@@ -15,11 +15,30 @@ namespace HardCodedDocuSignAdapter
         {
             ILog log = LogManager.GetLogger(typeof(Program));
             log.Info("Started the program.");
-            string statusMsg;
-            List<DocInfo> documents = new List<DocInfo>();
-            bool bResponse = SignDocument.SendForSigning("my_name", "my_email_id", documents, out statusMsg);
-            Console.WriteLine("The response from DocuSignPOC was: " + bResponse + " and the status message was: " + statusMsg);
+
+            //MakeRequest();
+            CopyDocuSignExample.Hello();
+            
             Console.ReadKey();
+        }
+
+        private static void MakeRequest()
+        {
+            string customerName = "Anurag Kalra";
+            string customerEmail = "anurag.kalra@safeauto.com";
+
+            DocInfo document1 = new DocInfo("GA_Application", "74f811c9-d8f2-4c9a-920c-70b7cf4e0348", "pol1000");
+            DocInfo document2 = new DocInfo("GA_Excluded_Driver_Form", "b7e31bd0-1303-4d2c-aa7d-f2b04fd82bf5", "pol1000");
+            DocInfo document3 = new DocInfo("GA_UM_Selection_Form", "b57163df-fb47-4e0b-b21c-9654ab77c651", "pol1000");
+            List<DocInfo> documents = new List<DocInfo>();
+            documents.Add(document1);
+            documents.Add(document2);
+            documents.Add(document3);
+
+            string statusMsg;
+            bool bResponse = SignDocument.SendForSigning(customerName, customerEmail, documents, out statusMsg);
+            Console.WriteLine("The response from DocuSignPOC was: " + bResponse + " and the status message was: " + statusMsg);
+
         }
     }
 }
